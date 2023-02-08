da6e3ed421540ada584dfd136d69a632faec8229
Paulus Schoutsen <balloob@gmail.com>
2023-02-08 13:40:51 -0500

    Split build tooling

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,35 @@
-import html from '@web/rollup-plugin-html';
-import esbuild from 'rollup-plugin-esbuild';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import { string } from 'rollup-plugin-string';
-import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/index.ts',
+  input: 'dist/index.js',
   output: {
-    dir: 'dist',
-    format: 'cjs',
+    dir: 'dist/web',
+    format: 'module',
   },
-  external: [
-    'lit',
-    'lit/decorators.js',
-    'lit/directives/class-map.js',
-    '@material/mwc-button',
-    '@material/mwc-icon-button',
-    '@material/mwc-linear-progress',
-    '@material/mwc-circular-progress',
-    '@material/mwc-formfield',
-    '@material/mwc-radio',
-    '@material/mwc-dialog',
-    '@mdi/js',
-  ],
+  preserveEntrySignatures: false,
   plugins: [
-    typescript(),
+    nodeResolve(),
+    babel({
+      babelHelpers: 'bundled',
+      plugins: ['@babel/plugin-proposal-class-properties'],
+    }),
     string({
       include: '**/*.py',
     }),
   ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    terser({
+      ecma: 2019,
+      toplevel: true,
+      format: {
+        comments: false,
+      },
+    })
+  );
+}
