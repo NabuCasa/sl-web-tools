be28ab04a8cdf1d03131b7b53dbf8c7f8ff3f0ff
puddly <32534428+puddly@users.noreply.github.com>
2023-03-17 17:05:33 -0400

    Override with `application_baudrate` when loading the manifest

--- src/nabucasa-zigbee-flasher.ts
+++ src/nabucasa-zigbee-flasher.ts
@@ -18,6 +18,10 @@ export class NabuCasaSilabsFlasher extends LitElement {
       console.error(
         'The `application_baudrate` manifest attribute is deprecated and has been replaced with `cpc_baudrate`, `ezsp_baudrate`, and `spinel_baudrate`. It will be removed in a future release, please update your manifest.'
       );
+
+      manifest.cpc_baudrate ??= manifest.application_baudrate;
+      manifest.ezsp_baudrate ??= manifest.application_baudrate;
+      manifest.spinel_baudrate ??= manifest.application_baudrate;
     }
 
     const dialog = document.createElement('flashing-dialog');
